1fd09e51cff6f739ae3bc8ffbe95df3e10a2d1a5
dosgo <dosgo@qq.com>
2016-06-06 22:08:30 +0800

    auto commot

--- dd.cpp
+++ dd.cpp
@@ -83,7 +83,6 @@ void get_local_addr(unsigned char* mac, u_int32_t &ip)
 int macfindip( char* dmacv)
 {
     printf("go1\r\n");
-    return -1;
     int timeout = 2;
     int optval = 1;
     int s; /* socket */
