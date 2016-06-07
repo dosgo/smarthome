6e1a1bc991f84a46eea419a62efefc658679d548
dosgo <dosgo@qq.com>
2016-06-07 15:14:54 +0800

    auto commot

--- main.cpp
+++ main.cpp
@@ -516,7 +516,7 @@ int getPidByName(char* task_name)
 void tolower(char *str)
 {
     int i=0;
-    for(i = 0; i < sizeof(str); i++)
+    for(i = 0; i <(int) sizeof(str); i++)
     {
            str[i] = tolower(str[i]);
     }
