70f6b3d27c2a117fab754e7d835d6c99d1d4db1e
dosgo <dosgo@qq.com>
2016-06-15 16:47:48 +0800

    auto commot

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int getPidByName(char* task_name);
 int getPidBySid(int sid,list<int>*pidlist);
 #endif
 int checktime=20;
-char VER[28]="v1.82-(2016/6/12)";
+char VER[28]="v1.83-(2016/6/15)";
 int FindIP(char *mac,char *ip);
 char backhomecmd[1024]="cmd.exe";//返回家
 char gohomecmd[1024]="cmd.exe";//离开家
