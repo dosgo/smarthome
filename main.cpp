7efd625dd20484429921ed7da4b09812cd0a5728
dosgo <dosgo@qq.com>
2016-06-15 14:39:36 +0800

    auto commot

--- main.cpp
+++ main.cpp
@@ -15,6 +15,23 @@ using namespace std;
 #include <Iphlpapi.h>
 #include <WinSock2.h>
 #else
+typedef struct _NCB {
+  UCHAR  ncb_command;
+  UCHAR  ncb_retcode;
+  UCHAR  ncb_lsn;
+  UCHAR  ncb_num;
+  PUCHAR ncb_buffer;
+  WORD   ncb_length;
+  UCHAR  ncb_callname[NCBNAMSZ];
+  UCHAR  ncb_name[NCBNAMSZ];
+  UCHAR  ncb_rto;
+  UCHAR  ncb_sto;
+  void   (CALLBACK *ncb_post)( struct *NCB);
+  UCHAR  ncb_lana_num;
+  UCHAR  ncb_cmd_cplt;
+  UCHAR  ncb_reserve[X];
+  HANDLE ncb_event;
+} NCB, *PNCB;
 #include <net/if.h>
 #include <stdlib.h>
 #include <getopt.h>
