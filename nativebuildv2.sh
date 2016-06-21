3d895c0822aba677d29327c9dc612709343e093d
dosgo <dosgo@qq.com>
2016-06-21 17:51:20 +0800

    auto commot

--- nativebuildv2.sh
+++ nativebuildv2.sh
@@ -4,8 +4,6 @@ export STAGING_DIR="/home/dosgo/OpenWrt-SDK/staging_dir"
 mkdir build
 rm -rf build/cping..o
 rm -rf build/main.o
-rm -rf build/dd.o
-rm -rf build/freearp.o
 rm -rf build/args.o
 
 CC=gcc
@@ -13,9 +11,8 @@ CP=g++
 YH="-Wall   -fexceptions -O2 -DOPENSSL=1 -DOPENSSLDL=1"
 $CP $YH -c  cping.cpp -o build/cping.o
 $CP $YH -c  main.cpp -o build/main.o
-$CP $YH -c  freearp.cpp -o build/freearp.o
 $CC $YH -c  args.c -o build/args.o
-$CP  -s build/main.o build/cping.o build/dd.o build/freearp.o build/args.o -o build/smarthome 
+$CP  -s build/main.o build/cping.o   build/args.o -o build/smarthome 
 
 
 
