ee759529dfc14145663492d117f0c6c8056c25ef
dosgo <dosgo@qq.com>
2016-06-25 18:08:11 +0800

    auto commot

--- main.cpp
+++ main.cpp
@@ -117,6 +117,7 @@ int main(int argc, char *argv[])
 
     while(true){
              CPing ping;
+             sleeps(20*1000);//ms
          int xx=ping.PingCheckV3("192.168.8.135");
         printf("xx:%d\r\n",xx);
         continue;
